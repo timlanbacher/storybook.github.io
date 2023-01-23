631ca7e884ecc155d964f778f5fd0594b4b3c709
Tim Lanbacher <tim.lanbacher@exanic.ch>
2023-01-23 14:08:07 +0100

    first version

---
+++ 61.1d5df921.iframe.bundle.js
@@ -0,0 +1,20 @@
+"use strict";
+(self["webpackChunktca_shared_ui"] = self["webpackChunktca_shared_ui"] || []).push([[61],{
+
+/***/ "./node_modules/@storybook/components/dist/syntaxhighlighter-6JBF4TYN.mjs":
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "SyntaxHighlighter": () => (/* reexport safe */ _chunk_XHUUYXNA_mjs__WEBPACK_IMPORTED_MODULE_0__.d3),
+/* harmony export */   "createCopyToClipboardFunction": () => (/* reexport safe */ _chunk_XHUUYXNA_mjs__WEBPACK_IMPORTED_MODULE_0__.xV),
+/* harmony export */   "default": () => (/* reexport safe */ _chunk_XHUUYXNA_mjs__WEBPACK_IMPORTED_MODULE_0__.qG)
+/* harmony export */ });
+/* harmony import */ var _chunk_XHUUYXNA_mjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./node_modules/@storybook/components/dist/chunk-XHUUYXNA.mjs");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=61.1d5df921.iframe.bundle.js.map
